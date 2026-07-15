14e52ba2ee0e8e08b80bfc908f011d7678031e94
Sivaram M <165845170+0xramm@users.noreply.github.com>
2026-07-15 19:00:50 +0530

    allow cjs file

--- scripts/generate-sitemap.cjs
+++ scripts/generate-sitemap.cjs
@@ -1,30 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const { pathToFileURL } = require('url');
+const fs = require("fs");
+const path = require("path");
+const { pathToFileURL } = require("url");
 
-const siteUrl = 'https://www.anshiyainnovations.com';
+const siteUrl = "https://www.anshiyainnovations.com";
 
 (async () => {
-  const { sitemapRoutes } = await import(pathToFileURL(path.resolve(__dirname, '../src/router/sitemapRoutes.js')).href);
+  const { sitemapRoutes } = await import(
+    pathToFileURL(path.resolve(__dirname, "../src/router/sitemapRoutes.js"))
+      .href
+  );
   const routes = sitemapRoutes;
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes
   .map(
-    (route) => `  <url>\n    <loc>${siteUrl}${route.path}</loc>\n    <changefreq>${route.changefreq}</changefreq>\n    <priority>${route.priority}</priority>\n  </url>`
+    (route) =>
+      `  <url>\n    <loc>${siteUrl}${route.path}</loc>\n    <changefreq>${route.changefreq}</changefreq>\n    <priority>${route.priority}</priority>\n  </url>`,
   )
-  .join('\n')}
+  .join("\n")}
 </urlset>
 `;
 
-  const distDir = path.resolve(__dirname, '../dist');
-  const publicDir = path.resolve(__dirname, '../public');
+  const distDir = path.resolve(__dirname, "../dist");
+  const publicDir = path.resolve(__dirname, "../public");
 
   fs.mkdirSync(distDir, { recursive: true });
   fs.mkdirSync(publicDir, { recursive: true });
 
-  fs.writeFileSync(path.join(distDir, 'sitemap.xml'), sitemap);
-  fs.writeFileSync(path.join(publicDir, 'sitemap.xml'), sitemap);
-  console.log('Sitemap generated at public/sitemap.xml and dist/sitemap.xml');
+  fs.writeFileSync(path.join(distDir, "sitemap.xml"), sitemap);
+  fs.writeFileSync(path.join(publicDir, "sitemap.xml"), sitemap);
+  console.log("Sitemap generated at public/sitemap.xml and dist/sitemap.xml");
 })();
