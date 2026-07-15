6f7485500962bfcb6802d03c0278f3d9d3103fbe
Sivaram M <165845170+0xramm@users.noreply.github.com>
2026-07-15 18:41:38 +0530

    seo: Sitemap Route update

--- src/router/sitemapRoutes.js
+++ src/router/sitemapRoutes.js
@@ -1,8 +1,8 @@
 export const sitemapRoutes = [
-  { path: '/', changefreq: 'weekly', priority: '1.0' },
-  { path: '/about-us', changefreq: 'monthly', priority: '0.9' },
-  { path: '/industries', changefreq: 'weekly', priority: '0.9' },
-  { path: '/career', changefreq: 'monthly', priority: '0.8' },
-  { path: '/contact', changefreq: 'monthly', priority: '0.8' },
-  { path: '/sap-service', changefreq: 'monthly', priority: '0.9' },
+  { path: "/", changefreq: "weekly", priority: "1.0" },
+  { path: "/about-us", changefreq: "monthly", priority: "0.9" },
+  { path: "/industries", changefreq: "weekly", priority: "0.9" },
+  { path: "/career", changefreq: "monthly", priority: "0.8" },
+  { path: "/contact", changefreq: "monthly", priority: "0.8" },
+  { path: "/sap-service", changefreq: "monthly", priority: "0.9" },
 ];
